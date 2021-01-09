5716026216bef495fc7bd48fbfdb7c5c5bd80073
aaham <aamir.s.ahamad@gmail.com>
2021-01-09 09:17:40 -0500

    added start script

--- app.js
+++ app.js
@@ -163,6 +163,8 @@ app.use((err, req, res, next)=>{
     res.status(statusCode).render('error', {err});
 })
 
-app.listen(5000,()=>{
-    console.log("serving port 5000")
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log(`serving on port ${port}`)
 })
